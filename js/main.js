f7bb6da0a2f549c97f4dd35432bdd0bad552d6c7
vikok <vikskopkop@gmail.com>
2016-12-26 16:24:53 +0200

    add map

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
 ;(function() {
 	"use strict";
 
+  window.onload = function() {
 
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.querySelector('.ba-map'), {
+      zoom: 7,
+      center: {lat: 49.588267, lng: 34.551417}
+  });
+  directionsDisplay.setMap(map);
 
+  var onChangeHandler = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('start').addEventListener('change', onChangeHandler);
+  document.getElementById('end').addEventListener('change', onChangeHandler);
+  };
+
+  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+    origin: document.getElementById('start').value,
+    destination: document.getElementById('end').value,
+    travelMode: google.maps.TravelMode.DRIVING
+  },
+  function(response, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
 })();
